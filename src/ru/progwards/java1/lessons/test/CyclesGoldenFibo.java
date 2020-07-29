e7df9a14239783fe1a78dff5ac3828d7ea0197a0
Holynlike <tercespot@list.ru>
2020-07-29 21:30:09 +0500

    29 июля, H4 Домашнее задание выполнено 100%

--- src/ru/progwards/java1/lessons/test/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/test/CyclesGoldenFibo.java
@@ -3,7 +3,19 @@ package ru.progwards.java1.lessons.test;
 public class CyclesGoldenFibo {
     public static void main(String[] args) {
         System.out.println(containsDigit(242, 3)); // Тестил, работает
-        System.out.println(fiboNumber(6));
+        System.out.println(fiboNumber(10)); // Тестил, работает
+        System.out.println(isGoldenTriangle(1,2,3)); // Тестил, работает
+        System.out.println(isGoldenTriangle(593,480,480)); // Тестил, работает
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(fiboNumber(i)); // Фибо - цикл
+        }
+        for (int i = 1; i < 100; i++) { // Цикл поиска золотых треугольников
+            for (int j = 1; j <= i; j++) { // Стремление к оптимизации цикла!  :)
+                if (isGoldenTriangle(i,j,j)){
+                    System.out.println("Найден золотой треугольник с параметрами: " + i + ", " + j + ", " + j);
+                }
+            }
+        }
     }
     public static boolean containsDigit(int number, int digit){
         System.out.println("Вызвана функция containsDigit с агрументами: " + number + ", " + digit);
@@ -18,18 +30,25 @@ public class CyclesGoldenFibo {
     }
     public static int fiboNumber(int n){ // Найти число Фибоначчи для n
         int RESfiboNumber = 0;
-        int fiboprev1 = 0, fiboprev2 = 0;
+        int fiboprev1 = 1, fiboprev2 = 1;
         if (n == 0 | n == 1){
             RESfiboNumber = 1;
         } else {
-            fiboprev1 = 0;
-            fiboprev2 = 1;
-            for (int i = 2; i == n; i++) {
+            for (int i = 2; i <= n; i++) {
                 RESfiboNumber = fiboprev1 + fiboprev2;
-                fiboprev1 = RESfiboNumber;
                 fiboprev2 = fiboprev1;
+                fiboprev1 = RESfiboNumber;
+                //System.out.printf("i=%d, RESfiboNumber=%d;  ", i, RESfiboNumber);
             }
         }
         return RESfiboNumber;
+    }
+    public static boolean isGoldenTriangle(int a, int b, int c){
+        boolean BLNisGoldenTriangle = false;
+        double Da = (double) (b + c) /a, Db = (double) (a + c) /b, Dc = (double) (b + a) /c;
+        if (a == b & Dc <= 1.61903 & Dc >= 1.61703 | c == b & Da <= 1.61903 & Da >= 1.61703 | a == c & Db <= 1.61903 & Db >= 1.61703) {
+            BLNisGoldenTriangle = true;
+        }
+        return BLNisGoldenTriangle;
     }
 }
