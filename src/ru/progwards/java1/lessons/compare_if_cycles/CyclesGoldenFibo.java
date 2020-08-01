69c097653818bdd78c037c2aeab34332076bc9ae
Holynlike <tercespot@list.ru>
2020-08-01 16:43:44 +0500

    01 августа, H4 Домашнее задание переделывыайу )

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -4,16 +4,17 @@ public class CyclesGoldenFibo {
     public static void main(String[] args) {
         System.out.println(containsDigit(242, 3)); // Тестил, работает
         System.out.println(containsDigit(12345, 1)); // Тестил, работает
+        System.out.println(containsDigit(0, 0)); // Тестил, работает
         System.out.println(fiboNumber(10)); // Тестил, работает
         System.out.println(isGoldenTriangle(1,2,3)); // Тестил, работает
-        System.out.println(isGoldenTriangle(593,480,480)); // Тестил, работает
+        System.out.println(isGoldenTriangle(55,55,34)); // Тестил, работает
         for (int i = 1; i <= 15; i++) {
             System.out.println(i + " - " + fiboNumber(i)); // Фибо - цикл
         }
         for (int i = 1; i < 100; i++) { // Цикл поиска золотых треугольников
             for (int j = 1; j <= i; j++) { // Стремление к оптимизации цикла!  :)
-                if (isGoldenTriangle(i,j,j)){
-                    System.out.println("Найден золотой треугольник с параметрами: " + i + ", " + j + ", " + j);
+                if (isGoldenTriangle(i,i,j)){
+                    System.out.println("Найден золотой треугольник с параметрами: " + i + ", " + i + ", " + j);
                 }
             }
         }
@@ -28,26 +29,19 @@ public class CyclesGoldenFibo {
     }
     public static int fiboNumber(int n){ // Найти число Фибоначчи для n
         int RESfiboNumber = 0;
-        int fiboprev1 = 0, fiboprev2 = 1;
-        if (n == 0 ){
-            RESfiboNumber = 1;
-        } else {
-            //fiboprev1 = 0;
-            for (int i = 1; i <= n; i++) {
+        int fiboprev1 = 0, fiboprev2 = 1; // Переменные для двух предыдущих значений
+        if (n == 0 | n == 1){
+            RESfiboNumber = n; // Для 0 и 1 Фибоначчи совпадает с n
+        } else { // Для бОльших чисел организуем "ханойский" цикл
+            for (int i = 2; i <= n; i++) {
                 RESfiboNumber = fiboprev1 + fiboprev2;
                 fiboprev1 = fiboprev2;
                 fiboprev2 = RESfiboNumber;
-                //System.out.printf("i=%d, RESfiboNumber=%d;  ", i, RESfiboNumber);
             }
         }
         return RESfiboNumber;
     }
     public static boolean isGoldenTriangle(int a, int b, int c){
-        boolean BLNisGoldenTriangle = false;
-        double Da = (double) (b + c) /a, Db = (double) (a + c) /b, Dc = (double) (b + a) /c;
-        if (a == b & Dc <= 1.61903 & Dc >= 1.61703 | c == b & Da <= 1.61903 & Da >= 1.61703 | a == c & Db <= 1.61903 & Db >= 1.61703) {
-            BLNisGoldenTriangle = true;
-        }
-        return BLNisGoldenTriangle;
+        return (a == b & (double)a/c >= 1.61703d & (double)a/c <= 1.61903d | a == c & (double)a/b  >= 1.61703d & (double)a/b  <= 1.61903d | b == c & (double)b/a  >= 1.61703d & (double)b/a  <= 1.61903d);
     }
 }
