695b443f6a3c475ef0c87f2995712b9e6b405592
Игорь <tercespot@list.ru>
2020-08-01 18:41:49 +0500

    01 августа, H4 Домашнее задание переделал

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,17 +1,13 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
+    private static int[] FIBOarr = new int[16]; // Массив для чисел Фибоначчи
+
     public static void main(String[] args) {
-        System.out.println(containsDigit(242, 3)); // Тестил, работает
-        System.out.println(containsDigit(12345, 1)); // Тестил, работает
-        System.out.println(containsDigit(0, 0)); // Тестил, работает
-        System.out.println(fiboNumber(10)); // Тестил, работает
-        System.out.println(isGoldenTriangle(1,2,3)); // Тестил, работает
-        System.out.println(isGoldenTriangle(55,55,34)); // Тестил, работает
         for (int i = 1; i <= 15; i++) {
             System.out.println(i + " - " + fiboNumber(i)); // Фибо - цикл
         }
-        for (int i = 1; i < 100; i++) { // Цикл поиска золотых треугольников
+        for (int i = 1; i < 100; i++) {    // Цикл поиска золотых треугольников
             for (int j = 1; j <= i; j++) { // Стремление к оптимизации цикла!  :)
                 if (isGoldenTriangle(i,i,j)){
                     System.out.println("Найден золотой треугольник с параметрами: " + i + ", " + i + ", " + j);
@@ -19,29 +15,31 @@ public class CyclesGoldenFibo {
             }
         }
     }
-    public static boolean containsDigit(int number, int digit){
-
-        System.out.println("Вызвана функция containsDigit с агрументами: " + number + ", " + digit);
-        //  будет возвращать true, если число number содержит цифру digit.
-        String Stext = Integer.toString(number); // число number переведено в строку Stext
-        String Sdigit = Integer.toString(digit); // число digit переведено в строку Sdigit
-        return Stext.contains(Sdigit); //Java String contains()- метод чтобы проверить, содержит ли String указанную последовательность
+    public static boolean containsDigit(int number, int digit){ //  будет возвращать true, если число number содержит цифру digit.
+        return Integer.toString(number).contains(Integer.toString(digit));
     }
     public static int fiboNumber(int n){ // Найти число Фибоначчи для n
+        // Оптимальнее всего будет сохранять найденные числа в общий для CyclesGoldenFibo массив
+        // и если n-й элемент массива не равен 0, то брать ответ из массива, так избегая избыточное использование цикла
         int RESfiboNumber = 0;
         int fiboprev1 = 0, fiboprev2 = 1; // Переменные для двух предыдущих значений
         if (n == 0 | n == 1){
             RESfiboNumber = n; // Для 0 и 1 Фибоначчи совпадает с n
         } else { // Для бОльших чисел организуем "ханойский" цикл
-            for (int i = 2; i <= n; i++) {
-                RESfiboNumber = fiboprev1 + fiboprev2;
-                fiboprev1 = fiboprev2;
-                fiboprev2 = RESfiboNumber;
+            if (FIBOarr[n]==0){ // Если искомого ещё нет в массиве, вычисляем число Фибоначчи.
+                for (int i = 2; i <= n; i++) {
+                    RESfiboNumber = fiboprev1 + fiboprev2;
+                    if (FIBOarr[i] == 0) FIBOarr[i] = RESfiboNumber; // Наполняем массив FIBOarr
+                    fiboprev1 = fiboprev2;
+                    fiboprev2 = RESfiboNumber;
+                }
+            } else{
+                RESfiboNumber = FIBOarr[n]; // Если в массиве уже есть найденное число Фибоначчи, просто без цикла выбираем его
             }
         }
         return RESfiboNumber;
     }
-    public static boolean isGoldenTriangle(int a, int b, int c){
+    public static boolean isGoldenTriangle(int a, int b, int c){//Хочется уложить функцию в одну строку
         return (a == b & (double)a/c >= 1.61703d & (double)a/c <= 1.61903d | a == c & (double)a/b  >= 1.61703d & (double)a/b  <= 1.61903d | b == c & (double)b/a  >= 1.61703d & (double)b/a  <= 1.61903d);
     }
 }
