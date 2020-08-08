552bf32f2558f2fb1dce4f384155076bc61feb0b
Holynlike <tercespot@list.ru>
2020-08-09 04:10:38 +0500

    09 августа, H7 Домашнее задание, отправляю на проверку...

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -3,17 +3,11 @@ package ru.progwards.java1.lessons.bitsworld;
 public class SumBits {
 
     public static int sumBits(byte value) {
-        int grade = 4;
-        if (value > 15 || value < -15) {
-            grade = 8;
-        }
         int result = 0;
-        int result2;
-        result2 = value;
-
-        for (int i = 0; i < grade; i++) {
+        int result2 = value;
+        for (int i = 0; i < 8; i++) {
             result += result2 & 1;
-            result2 = result2 >>> 1;
+            result2 = result2 >> 1;
         }
         return result;
     }
