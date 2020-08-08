614af7bbf7a2cbe04478d09b24b1293a27b96e95
Holynlike <tercespot@list.ru>
2020-08-08 17:07:50 +0500

    08 августа, H6 Домашнее задание, отправляю на проверку...

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -1,11 +1,8 @@
 package ru.progwards.java1.lessons.arrays;
-
 import java.util.Arrays;
-
 public class DIntArray {
     private int[] arr1 = new int[0];
     private int[] arr2;
-
     public DIntArray() {
     } // Конструктор по-умолчанию
 
@@ -17,13 +14,13 @@ public class DIntArray {
         arr1[arr1.length - 1] = num;  // наконец, в первом прописываю в последний элемент значение num
     }
 
-    public void atInsert(int pos, int num) { // Добавить элемент num в pos - позицию в массиве
+    public void atInsert(int pos, int num) {     // Добавить элемент num в pos - позицию в массиве
         int[] arr3 = new int[arr1.length - pos]; //  создадим новый, буферный массив.
         for (int i = 0; i < arr3.length; i++) {
             arr3[i] = arr1[pos + i];
         }
         arr2 = new int[arr1.length];
-        arr2 = Arrays.copyOf(arr1, arr1.length);// во второй массив копирую первый
+        arr2 = Arrays.copyOf(arr1, arr1.length); // во второй массив копирую первый
         arr1 = new int[arr1.length + 1]; // меняю размер первого массива
 
         for (int i = 0; i < pos - 1; i++) {
@@ -35,7 +32,7 @@ public class DIntArray {
         }
     }
 
-    public void atDelete(int pos) { // Удаление значения в позиции pos
+    public void atDelete(int pos) {  // Удаление значения в позиции pos
         arr2 = new int[arr1.length]; // копия массива
         arr2 = Arrays.copyOf(arr1, arr1.length);
         arr1 = new int[arr1.length - 1];
@@ -53,13 +50,13 @@ public class DIntArray {
         } else {
             return arr1[pos];
         }
-    }
+    }// Добавлю пару тестировочных функций:
 
     public int myLen() {
         return arr1.length;
-    }
+    } // - для показа количества элементов в массиве
 
     public void Show() {
         System.out.println(Arrays.toString(arr1));
-    }
+    }//для вывода всего массива в консольку
 }
