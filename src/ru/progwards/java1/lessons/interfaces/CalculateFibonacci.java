20873c893a23ea5fe0dfa50aff25e7bbc22fc029
Holynlike <tercespot@list.ru>
2020-08-31 18:57:14 +0500

    31 августа, H8 Домашнее задание, Добил!

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -3,18 +3,14 @@ package ru.progwards.java1.lessons.interfaces;
 public class CalculateFibonacci {
     private static CacheInfo lastFibo = new CacheInfo(0, 0);
     public static CacheInfo getLastFibo() {
-//        CacheInfo CI = new CacheInfo(0, 0);
-//        if (lastFibo == null) {
-//            return CI;
-//        } else {
-//            return lastFibo;
-//        }
-        return lastFibo;
+        if (lastFibo == null) {
+           return null;
+        } else {
+            return lastFibo;
+        }
     }
 
-    public static void clearLastFibo() {
-        lastFibo = null;
-    }
+    public static void clearLastFibo() {lastFibo = null;}
 
     public static int fiboNumber(int n) {
         int fiboprev1 = 0, fiboprev2 = 1, RESfiboNumber = 0;
@@ -41,7 +37,6 @@ public class CalculateFibonacci {
 
     public static class CacheInfo {
         public int n, fibo; // служебные переменные
-
         public CacheInfo(int n, int fibo) {
             this.n = n; // сохранение значений в переменных
             this.fibo = fibo;
