9de6c222d32afb955ac667f913d0ddb54eff92bb
Holynlike <tercespot@list.ru>
2020-11-16 22:18:17 +0500

    Попытка восстановить старый рабочий вариант

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -1,16 +1,7 @@
 package ru.progwards.java1.lessons.io2;
 
-import java.io.RandomAccessFile;
-
 public class PhoneNumber {
-    public static void main(String[] args) {
-        // Получить на выходе +7(999)111-2233
-        System.out.println(format("- 79991112233"));
-        System.out.println(format("8(999)111-22-33"));
-        System.out.println(format("8 999 111 22 33"));
-    }
-
-    public static String format(String phone) {
+public static String format(String phone) {
         String result = phone.replaceAll("[\\D]", ""); // Регуляркой сносим всё, кроме чисел
         if(result.length() == 11){ result = "+"+result;} // Если символов 11, добавляем плюс
         result = result.replace("+8", "+7");
